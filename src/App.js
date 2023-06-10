3cba7b0008c1ebf5e6c9e04d92894672c313be16
Chockalingam <padma.chockalingam@capgemini.com>
2023-06-10 08:09:10 +0530

    React chat bot implemented intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Chatbot from "react-chatbot-kit";
+import "react-chatbot-kit/build/main.css";
+import config from "./config";
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
+import { Icon } from "@iconify/react";
+import robotIcon from "@iconify-icons/mdi/wechat";
 
 function App() {
+  const [showBot, toggleBot] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-chatbot-container">
+        <div style={{ maxWidth: "500px" }}>
+          {showBot && (
+            <Chatbot
+              config={config}
+              botAvator={
+                <Icon
+                  icon="bx:bot"
+                  // icon={robotIcon}
+                  className="app-chatbot-button-icon"
+                  style={{ fontSize: "40", color: "white" }}
+                />
+              }
+              actionProvider={ActionProvider}
+              messageParser={MessageParser}
+              headerText={
+                <div>
+                  <span>Ask chatbot &nbsp;</span>
+                  <span className="app-chatbot-close-span">
+                    <a
+                      className="app-chatbot-close"
+                      href="#"
+                      onClick={() => toggleBot((prev) => !prev)}
+                    >
+                      x
+                    </a>
+                  </span>
+                </div>
+              }
+              placeholderText={"Type something..."}
+              close="true"
+            />
+          )}
+          <button
+            className="app-chatbot-button"
+            onClick={() => toggleBot((prev) => !prev)}
+          >
+            {/* <Tooltip
+          className="app-chatbox"
+          content={
+            <div>
+              <div className="app-chatbox">
+                Chat with Sara <br />
+              </div>
+            </div>
+          }
+          direction="left"
+          forceDirection
+        > */}
+            <div>
+              {
+                <Icon
+                  icon="bx:bot"
+                  // icon={robotIcon}
+                  className="app-chatbot-button-icon"
+                  style={{ fontSize: "40", color: "white" }}
+                />
+              }
+            </div>
+            {/* </Tooltip> */}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
